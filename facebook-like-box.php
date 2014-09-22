09c4541449bfe1a303d470d665b121e6807890a7
iamshehryar <iamshehryar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-22 05:20:14 +0000

    Updating new version
    git-svn-id: https://plugins.svn.wordpress.org/easy-facebook-like-box/trunk@994479 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-like-box.php
+++ facebook-like-box.php
@@ -1,149 +1,343 @@
 <?php
+
 /*
+
 Plugin Name: Easy Facebook Like Box
+
 Plugin URI: http://activebizsolutions.com/
+
 Description: Easy facebook like box display facebook like box. it enable Facebook Page owners to attract and gain Likes from their own website.
-Version: 1.0
+
+Version: 2.0
+
 Author: shehryar	
+
 Author URI: https://www.facebook.com/shehriyar.shehri	
+
 License: GPL2
+
 */
+
+
 
 class wp_my_plugin extends WP_Widget {
 
+
+
 	// constructor
+
 	function wp_my_plugin() {
+
 	parent::WP_Widget(false, $name = __('Easy Facebook Like Box', 'wp_widget_plugin') );
+
 		/* ... */
+
 	}
 
+
+
 	// widget form creation
+
 function form($instance) {
 
+
+
 // Check values
+
 if( $instance) {
+
      $title = esc_attr($instance['title']);
+
      $text = esc_attr($instance['text']);
+
      $width = esc_attr($instance['width']);
+
      $height = esc_attr($instance['height']);
+
      $face = esc_attr($instance['face']);
+
 	 $showheader = esc_attr($instance['showheader']);
+
 	 $showpost = esc_attr($instance['showpost']);
-	 $showborder = esc_attr($instance['border']);
+
+	 $showborder = esc_attr($instance['showborder']);
+
 	 $colorscheme = esc_attr($instance['colorscheme']);
 
+
+
 } else {
+
      $title = '';
+
      $text = '';
+
      $width = '';
+
      $height = '';
+
      $face = '';
+
 	 $showheader = '';
+
 	 $showpost = '';
+
 	 $showborder = '';
+
 	 $colorscheme = '';
+
 }
+
 ?>
 
+
+
 <p>
+
 <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Widget Title', 'wp_widget_plugin'); ?></label>
+
 <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+
 </p>
 
+
+
 <p>
+
 <label for="<?php echo $this->get_field_id('text'); ?>"><?php _e('Facebook page url:', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>" type="text" value="<?php echo $text; ?>" placeholder="Ex: activebizsolution" />
+
+<input class="widefat" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>" type="text" value="<?php echo $text; ?>" placeholder="Ex: http://www.facebook.com/activebizsolution" />
+
 </p>
 
+
+
 <p>
+
 <label for="<?php echo $this->get_field_id('width'); ?>"><?php _e('Width:', 'wp_widget_plugin'); ?></label>
+
 <input class="widefat" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo $width; ?>" placeholder="Ex: 300" />
+
 </p>
 
+
+
 <p>
+
 <label for="<?php echo $this->get_field_id('height'); ?>"><?php _e('height:', 'wp_widget_plugin'); ?></label>
+
 <input class="widefat" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo $height; ?>" placeholder="Ex: 400" />
+
 </p>
 
+
+
 <p>
+
 <label for="<?php echo $this->get_field_id('colorscheme'); ?>"><?php _e('Color Scheme', 'wp_widget_plugin'); ?></label>
+
 <select name="<?php echo $this->get_field_name('colorscheme'); ?>" id="<?php echo $this->get_field_id('colorscheme'); ?>" class="widefat">
+
 <?php
+
 $options = array('light', 'dark');
+
 foreach ($options as $option) {
+
 echo '<option value="' . $option . '" id="' . $option . '"', $colorscheme == $option ? ' selected="selected"' : '', '>', $option, '</option>';
+
 }
+
 ?>
+
 </select>
+
 </p>
 
+
+
 <p>
+
 <input id="<?php echo $this->get_field_id('face'); ?>" name="<?php echo $this->get_field_name('face'); ?>" type="checkbox" value="1" <?php checked( '1', $face ); ?> />
+
 <label for="<?php echo $this->get_field_id('face'); ?>"><?php _e('Show Faces', 'wp_widget_plugin'); ?></label>
 
+
+
 <input id="<?php echo $this->get_field_id('showheader'); ?>" name="<?php echo $this->get_field_name('showheader'); ?>" type="checkbox" value="1" <?php checked( '1', $showheader ); ?> />
+
 <label for="<?php echo $this->get_field_id('showheader'); ?>"><?php _e('Show Header', 'wp_widget_plugin'); ?></label>
+
 </p>
+
 <p>
+
 <input id="<?php echo $this->get_field_id('showpost'); ?>" name="<?php echo $this->get_field_name('showpost'); ?>" type="checkbox" value="1" <?php checked( '1', $showpost ); ?> />
+
 <label for="<?php echo $this->get_field_id('showpost'); ?>"><?php _e('Show Post', 'wp_widget_plugin'); ?></label>
 
+
+
 <input id="<?php echo $this->get_field_id('showborder'); ?>" name="<?php echo $this->get_field_name('showborder'); ?>" type="checkbox" value="1" <?php checked( '1', $showborder ); ?> />
+
 <label for="<?php echo $this->get_field_id('showborder'); ?>"><?php _e('Show Border', 'wp_widget_plugin'); ?></label>
+
 </p>
+
+
+
 
 
 <?php
+
 }
+
+
 
 	// update widget
+
 function update($new_instance, $old_instance) {
+
       $instance = $old_instance;
+
       // Fields
+
       $instance['title'] = strip_tags($new_instance['title']);
+
       $instance['text'] = strip_tags($new_instance['text']);
+
       $instance['width'] = strip_tags($new_instance['width']);
+
       $instance['height'] = strip_tags($new_instance['height']);
+
 	  $instance['face'] = strip_tags($new_instance['face']);
+
 	  $instance['showheader'] = strip_tags($new_instance['showheader']);
+
 	  $instance['showpost'] = strip_tags($new_instance['showpost']);
+
 	  $instance['showborder'] = strip_tags($new_instance['showborder']);
+
 	  $instance['colorscheme'] = strip_tags($new_instance['colorscheme']);
+
      return $instance;
+
 }
+
+
 
 	// display widget
+
 function widget($args, $instance) {
+
    extract( $args );
+
    // these are the widget options
+
    $title = apply_filters('widget_title', $instance['title']);
+
    $text = $instance['text'];
+
    $width = $instance['width'];
+
    $height = $instance['height'];
+
    $face = $instance['face'];
+
    $showheader = $instance['showheader'];
+
    $showpost = $instance['showpost'];
+
    $showborder = $instance['showborder'];
+
    $colorscheme = $instance['colorscheme'];
+
    echo $before_widget;
+
    // Display the widget
+
    echo '<div class="widget-text wp_widget_plugin_box">';
 
+
+
    // Check if title is set
+
    if ( $title ) {
+
       echo $before_title . $title . $after_title;
+
    }
+
+
 
    // Check if text is set
+
    if( $text ) {
-      echo '<p class="wp_widget_plugin_text">'.'<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2F'.$text.'&amp;width';if( $width ) { echo '='.$width; } echo '&amp;height';if( $height ) { echo '='.$height; } echo '&amp;colorscheme=';if ( $colorscheme == 'light' ) { echo 'light';} else if ( $colorscheme == 'dark' ) { echo 'dark';} echo '&amp;show_faces';if( $face AND $face == '1' ) { echo '='.'true'; } echo '&amp;header';if( $showheader AND $showheader == '1' ) { echo '='.'true'; } echo'&amp;stream';if( $showpost AND $showpost == '1' ) { echo '='.'true'; } echo '&amp;show_border';if( $showborder AND $showborder == '1' ) { echo '='.'true'; } echo '&amp;appId=454074158008443" scrolling="no" frameborder="0" style="border:none; overflow:hidden;';if( $width ) { echo 'width:'.$width.'px;'; } if($height ) { echo 'height:'.$height.'px;'; } echo '" allowTransparency="true"></iframe></p>';
+	   
+	   if( $face == '1' ) { $face = 'true'; } else {$face = 'false';}
+	   if( $showheader == '1' ) { $showheader = 'true'; } else {$showheader = 'false';}
+	   if( $showpost == '1' ) { $showpost = 'true'; } else {$showpost = 'false';}
+	   if( $showborder == '1' ) { $showborder = 'true'; } else {$showborder = 'false';}
+
+      echo '<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=454074158008443&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, "script", "facebook-jssdk"));</script>'.'<div class="fb-like-box" data-href="'.$text.'" data-width="'.$width.'" data-height="'.$height.'" data-colorscheme="'.$colorscheme.'" data-show-faces="'.$face.'" data-header="'.$showheader.'" data-stream="'.$showpost.'" data-show-border="'.$showborder.'"></div>';
+
    }
+
    
+
    echo '</div>';
+
    echo $after_widget;
-}
+
 }
 
+}
+
+
+
 // register widget
+
 add_action('widgets_init', create_function('', 'return register_widget("wp_my_plugin");'));
+function easy_facebook_like_box_functiona($atts){
+   extract(shortcode_atts(array(
+	  'url' => 'www.facebook.com/FacebookDevelopers',
+	  'width' => '',
+	  'height' => '',
+	  'theme' => 'light',
+	  'faces' => 'true',
+	  'header' => 'true',
+	  'posts' => 'true',
+	  'border' => 'true',
+   ), $atts));
+
+   $return_string = '<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=454074158008443&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, "script", "facebook-jssdk"));</script>';
+  
+   $return_string .= '<div class="fb-like-box" data-href="'.$url.'" data-width="'.$width.'" data-height="'.$height.'" data-colorscheme="'.$theme.'" data-show-faces="'.$faces.'" data-header="'.$header.'" data-stream="'.$posts.'" data-show-border="'.$border.'"></div>';
+   if ($width) {
+	     
+              }
+
+
+   wp_reset_query();
+   return $return_string;
+}
+function easy_facebook_shortcodess(){
+   add_shortcode('easy-fb-like-box', 'easy_facebook_like_box_functiona');
+}
+add_action( 'init', 'easy_facebook_shortcodess');
+
 ?>
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: iamshehryar
 Tags: buddypress, Facebook, facebook like, facebook like box, facebook like button, fb like, wordpress facebook like box, facebook like for wordpress
 Requires at least: 2.8.6
-Tested up to: 3.9.1
-Stable tag: 1.0
+Tested up to: 4.0
+Stable tag: 2.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -23,6 +23,11 @@ Easy facebook like box display facebook like box. it enable Facebook Page owners
 6. Stream - choose show or hide lastest posts from your facebook fan page.
 7. Header - choose show or hide header.
 8. Border - choose show or hide border around the facebook like box.
+
+= Shortcode =
+
+[easy-fb-like-box url="enter your facebook page url" width="set your width" height="set your height" theme="choose one theme light or dark" faces="choose true or false" header="choose ture or false" posts="choose true or false" border="choose true or false"]
+
 
 == Installation ==
 
